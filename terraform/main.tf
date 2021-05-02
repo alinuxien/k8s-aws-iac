4ced52b3076ccb794c5b9a0abe2e483d830abd19
Ali Akrour <ali.akrour@gmail.com>
2021-05-02 20:46:27 +0200

    Starts Terraform Config - Credentials Management

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,16 @@
 provider "aws" {
-  region = "eu-west-3"
-}
-
-provider "aws" {
-  region = "us-east-1"
-  alias  = "use1"
+  region = ${TF_VAR_AWS_DEFAULT_REGION}
+  access_key = ${TF_VAR_AWS_ACCESS_KEY_ID}
+  secret_key = ${TF_VAR_AWS_SECRET_ACCESS_KEY}
 }
 
 data "aws_availability_zones" "available" {}
 
 data "local_file" "local-pub-key" {
-  filename = var.public_key_file
+  filename = ${TF_VAR_AWS_SSH_PUBLIC_KEY_FILENAME}
 }
 
-data "aws_acm_certificate" "cloudfront" {
-  provider = aws.use1
-  domain   = var.domain
-}
-
-data "aws_acm_certificate" "cert" {
-  domain = var.domain
-}
-
-data "aws_route53_zone" "primary" {
-  name = var.domain
-}
-
-resource "aws_key_pair" "keypair" {
-  key_name   = "keypair"
+resource "aws_key_pair" "aws_k8s" {
+  key_name   = "aws_k8s_keypair"
   public_key = data.local_file.local-pub-key.content
 }
