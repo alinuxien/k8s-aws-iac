9b0f439078302ca20bb353be483bd0586c36a512
Ali Akrour <ali.akrour@gmail.com>
2021-05-02 20:48:29 +0200

    Debug credentials env vars access

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = ${TF_VAR_AWS_DEFAULT_REGION}
-  access_key = ${TF_VAR_AWS_ACCESS_KEY_ID}
-  secret_key = ${TF_VAR_AWS_SECRET_ACCESS_KEY}
+  region = ${env.TF_VAR_AWS_DEFAULT_REGION}
+  access_key = ${env.TF_VAR_AWS_ACCESS_KEY_ID}
+  secret_key = ${env.TF_VAR_AWS_SECRET_ACCESS_KEY}
 }
 
 data "aws_availability_zones" "available" {}
 
 data "local_file" "local-pub-key" {
-  filename = ${TF_VAR_AWS_SSH_PUBLIC_KEY_FILENAME}
+  filename = ${env.TF_VAR_AWS_SSH_PUBLIC_KEY_FILENAME}
 }
 
 resource "aws_key_pair" "aws_k8s" {
