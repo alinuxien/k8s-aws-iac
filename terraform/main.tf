6a2eecc7ff90b9768c431a88213c0d5398b5c253
Ali Akrour <ali.akrour@gmail.com>
2021-05-08 09:43:46 +0200

    Prepares bastion addition branch

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,3 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
-data "local_file" "local-pub-key" {
-  filename = var.public_key_file
-}
-
-resource "aws_key_pair" "keypair" {
-  key_name   = "keypair"
-  public_key = data.local_file.local-pub-key.content
-}
-
