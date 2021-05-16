c0ff8ab489042b117854c717de522016765a7cc4
Ali Akrour <ali.akrour@gmail.com>
2021-05-16 14:59:20 +0000

    changes bastion & nodes AMI to Ubuntu 20.04 LTS

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -7,13 +7,13 @@ variable "project_name" {
 variable "ami-bastion" {
   type        = string
   description = "Image Amazon à utiliser pour le Bastion SSH"
-  default     = "ami-0de12f76efe134f2f"
+  default     = "ami-0f7cd40eac2214b37"
 }
 
 variable "bastion-user" {
   type        = string
   description = "Nom utilisateur SSH pour le bastion"
-  default     = "ec2-user"
+  default     = "ubuntu"
 }
 
 variable "instance-type-bastion" {
@@ -35,13 +35,13 @@ variable "private_key_file" {
 variable "ami-k8s-nodes" {
   type        = string
   description = "Image Amazon à utiliser pour les nodes Kubernetes"
-  default     = "ami-0de12f76efe134f2f"
+  default     = "ami-0f7cd40eac2214b37"
 }
 
 variable "k8s-nodes-user" {
   type        = string
   description = "Nom utilisateur SSH pour les nodes Kubernetes"
-  default     = "ec2-user"
+  default     = "ubuntu"
 }
 
 variable "instance-type-k8s-node-master" {
