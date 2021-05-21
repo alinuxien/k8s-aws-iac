c686d7c2aa8f338ec9fe4a2d60491a62b0e06a54
Ali Akrour <ali.akrour@gmail.com>
2021-05-21 23:11:28 +0000

    Fix terraform formatting

--- terraform/ansible-nodes-master.tf
+++ terraform/ansible-nodes-master.tf
@@ -7,7 +7,7 @@ resource "null_resource" "plays_k8s-master" {
     command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/push-certs.yml"
   }
 
- depends_on = [
+  depends_on = [
     aws_nat_gateway.ngw-a,
     aws_instance.bastion,
     aws_instance.k8s-node-master-a,
