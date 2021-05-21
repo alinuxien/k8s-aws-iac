810ef469f2ed1f170ad8f9f4a8606deda24eac5e
Ali Akrour <ali.akrour@gmail.com>
2021-05-21 20:13:47 +0000

    removes nlb incompatible security group parameter

--- terraform/elb-n.tf
+++ terraform/elb-n.tf
@@ -1,55 +1,8 @@
-resource "aws_security_group" "lb" {
-  name        = "lb"
-  description = "Autoriser le traffic HTTP et HTTPS entrant"
-  vpc_id      = aws_vpc.main.id
-
-  tags = {
-    Name = "lb"
-  }
-}
-
-resource "aws_security_group_rule" "in_http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.lb.id
-}
-
-resource "aws_security_group_rule" "in_https" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.lb.id
-}
-
-resource "aws_security_group_rule" "out_http" {
-  type              = "egress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.lb.id
-}
-
-resource "aws_security_group_rule" "out_https" {
-  type              = "egress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.lb.id
-}
-
 resource "aws_lb" "lb" {
   name                       = "kubernetes"
   internal                   = false
   load_balancer_type         = "network"
   drop_invalid_header_fields = false
-  security_groups            = [aws_security_group.lb.id]
   subnets                    = [aws_subnet.public-a.id, aws_subnet.public-b.id]
 
   tags = {
