f0c35beabb7b6702bd8163ce305d229c57288026
Ali Akrour <ali.akrour@gmail.com>
2021-05-21 22:56:54 +0000

    moves work certs copy to separate ansible role

--- terraform/ansible-nodes-master.tf
+++ terraform/ansible-nodes-master.tf
@@ -3,7 +3,11 @@ resource "null_resource" "plays_k8s-master" {
     command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/prepare-certs.yml"
   }
 
-  depends_on = [
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/push-certs.yml"
+  }
+
+ depends_on = [
     aws_nat_gateway.ngw-a,
     aws_instance.bastion,
     aws_instance.k8s-node-master-a,
