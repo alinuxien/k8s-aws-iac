c6a81f8da1de3ba3948dd4f461aa3bcb517c4033
Ali Akrour <ali.akrour@gmail.com>
2021-05-23 13:42:43 +0000

    adds copy of all kubeconfigs to nodes

--- terraform/create-and-deploy-configs.tf
+++ terraform/create-and-deploy-configs.tf
@@ -2,6 +2,14 @@ resource "null_resource" "prepare-and-deploy-configs" {
   provisioner "local-exec" {
     command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/prepare-configs.yml"
   }
+  
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/push-configs-workers.yml"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/push-configs-controllers.yml"
+  }
 
   depends_on = [
     null_resource.prepare-and-deploy-certs,
