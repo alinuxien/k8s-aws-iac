04e8f175d22e0883030ea7caf590610595930ef8
Ali Akrour <ali.akrour@gmail.com>
2021-05-23 14:16:58 +0000

    adds encryption config file deployment

--- terraform/data-encryption-config-and-key.tf
+++ terraform/data-encryption-config-and-key.tf
@@ -3,6 +3,10 @@ resource "null_resource" "prepare-and-deploy-encryption" {
     command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/encryption.yml"
   }
 
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/push-encryption.yml"
+  }
+
   depends_on = [
     aws_instance.bastion,
     aws_instance.k8s-node-master-a,
