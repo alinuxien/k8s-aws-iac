dee48bbc4e67875151221e5194e71a3b5546564d
Ali Akrour <ali.akrour@gmail.com>
2021-06-02 22:41:09 +0000

    adds missing dependencies beetween nodes bootstrap and kubeconfig deployment

--- terraform/control-plane.tf
+++ terraform/control-plane.tf
@@ -5,6 +5,7 @@ resource "null_resource" "control-plane" {
 
   depends_on = [
     null_resource.prepare-and-deploy-certs,
+    null_resource.prepare-and-deploy-configs,
     local_file.AnsibleK8SControlPlane
   ]
 }
