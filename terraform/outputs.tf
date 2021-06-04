031b78bb95f27aa11a433dadfc0e5f6fdf7cec30
Ali Akrour <ali.akrour@gmail.com>
2021-06-04 16:13:14 +0000

    fix ansible become @task level by shell module with sudo

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -105,9 +105,10 @@ resource "local_file" "AnsibleK8SControlPlane" {
 
 resource "local_file" "AnsibleK8SWorkers" {
   content = templatefile("../ansible/roles/workers/tasks/main.tmpl", {
-    pod-cidr     = var.pod-cidr,
-    worker-0-dns = aws_instance.worker-0.private_dns,
-    worker-1-dns = aws_instance.worker-1.private_dns
+    k8s-nodes-user = var.k8s-nodes-user,
+    pod-cidr       = var.pod-cidr,
+    worker-0-dns   = aws_instance.worker-0.private_dns,
+    worker-1-dns   = aws_instance.worker-1.private_dns
   })
   filename = "../ansible/roles/workers/tasks/main.yml"
 }
