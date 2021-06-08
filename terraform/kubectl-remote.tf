bc566c8f8e72a5ed2fd051510610c9aa516dc1de
Ali Akrour <ali.akrour@gmail.com>
2021-06-08 17:54:30 +0000

    debug remote kubectl config

--- terraform/kubectl-remote.tf
+++ terraform/kubectl-remote.tf
@@ -10,23 +10,4 @@ resource "null_resource" "kubectl-remote" {
   ]
 }
 
-resource "null_resource" "cleaning-temp-certs-files" {
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/clean-certs.yml"
-  }
-
-  depends_on = [
-    null_resource.kubectl-remote
-  ]
-}
-
-resource "null_resource" "cleaning-temp-config-files" {
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/clean-configs.yml"
-  }
-
-  depends_on = [
-    null_resource.kubectl-remote
-  ]
-}
 
