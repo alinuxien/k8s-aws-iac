54217ea637fccd1cc53675884ecca70ca4e58c8c
Ali Akrour <ali.akrour@gmail.com>
2021-06-19 14:42:47 +0000

    adds flannel cni plugin to add pods network overlay between vpc subnets

---
+++ terraform/flannel-cni-plugin.tf
@@ -0,0 +1,13 @@
+resource "null_resource" "flannel-cni-plugin" {
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/flannel-cni-plugin.yml -e 'ansible_become=yes ansible_become_user=vagrant ansible_become_pass=vagrant'"
+  }
+
+  depends_on = [
+    null_resource.kubectl-remote,
+    aws_route.worker-1-pod-route-a,
+    aws_route.worker-0-pod-route-b
+  ]
+}
+
+
