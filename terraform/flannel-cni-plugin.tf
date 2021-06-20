7cb6661b02edccbf498525ba362bc2f4f797a1ed
Ali Akrour <ali.akrour@gmail.com>
2021-06-20 18:10:18 +0000

    makes flannel wait 30s after node bootstrap; reduce coredns wait

--- terraform/flannel-cni-plugin.tf
+++ terraform/flannel-cni-plugin.tf
@@ -1,13 +1,19 @@
-resource "null_resource" "flannel-cni-plugin" {
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/flannel-cni-plugin.yml -e 'ansible_become=yes ansible_become_user=vagrant ansible_become_pass=vagrant'"
-  }
-
+resource "time_sleep" "wait_30_seconds" {
   depends_on = [
     null_resource.kubectl-remote,
     aws_route.worker-1-pod-route-a,
     aws_route.worker-0-pod-route-b
   ]
+
+  create_duration = "30s"
+}
+
+resource "null_resource" "flannel-cni-plugin" {
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/flannel-cni-plugin.yml -e 'ansible_become=yes ansible_become_user=vagrant ansible_become_pass=vagrant'"
+  }
+
+  depends_on = [time_sleep.wait_30_seconds] 
 }
 
 
