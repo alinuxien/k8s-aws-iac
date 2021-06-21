cc2162242176ff9da78dfbc3760b254c19d796d3
Ali Akrour <ali.akrour@gmail.com>
2021-06-21 16:41:13 +0000

    makes workers wait 60s after controllers finished bootstrap & makes coredns waits 30s after remote kubectl setup finished ( itself depending on workers )

--- terraform/dns-cluster-add-on.tf
+++ terraform/dns-cluster-add-on.tf
@@ -1,13 +1,18 @@
-resource "null_resource" "dns-cluster-add-on" {
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/dns-cluster-add-on.yml -e 'ansible_become=yes ansible_become_user=vagrant ansible_become_pass=vagrant'"
-  }
-
+resource "time_sleep" "wait_30_seconds" {
   depends_on = [
     null_resource.kubectl-remote,
     aws_route.worker-1-pod-route-a,
     aws_route.worker-0-pod-route-b
   ]
+  create_duration = "30s"
+}
+
+resource "null_resource" "dns-cluster-add-on" {
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/dns-cluster-add-on.yml -e 'ansible_become=yes ansible_become_user=vagrant ansible_become_pass=vagrant'"
+  }
+
+  depends_on = [time_sleep.wait_30_seconds]
 }
 
 
--- terraform/workers.tf
+++ terraform/workers.tf
@@ -1,12 +1,18 @@
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [
+    null_resource.prepare-and-deploy-certs,
+    null_resource.prepare-and-deploy-configs,
+    null_resource.control-plane,
+    local_file.AnsibleK8SWorkers
+  ]
+  create_duration = "60s"
+}
+
 resource "null_resource" "workers-bootstraping" {
   provisioner "local-exec" {
     command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/workers.yml"
   }
 
-  depends_on = [
-    null_resource.prepare-and-deploy-certs,
-    null_resource.prepare-and-deploy-configs,
-    local_file.AnsibleK8SWorkers
-  ]
+  depends_on = [time_sleep.wait_60_seconds]
 }
 
