c52bf7fdd6dee62a7eebcddb9b5323eaff8b5247
Ali Akrour <ali.akrour@gmail.com>
2021-06-21 09:00:18 +0000

    adds 60 secs delay between calico and coredns

--- terraform/dns-cluster-add-on.tf
+++ terraform/dns-cluster-add-on.tf
@@ -1,9 +1,15 @@
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [null_resource.calico-cni-plugin]
+
+  create_duration = "60s"
+}
+
 resource "null_resource" "dns-cluster-add-on" {
   provisioner "local-exec" {
     command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/dns-cluster-add-on.yml -e 'ansible_become=yes ansible_become_user=vagrant ansible_become_pass=vagrant'"
   }
 
-  depends_on = [null_resource.calico-cni-plugin]
+  depends_on = [time_sleep.wait_60_seconds]
 }
 
 
