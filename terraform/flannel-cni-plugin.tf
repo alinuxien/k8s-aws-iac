db1d095fb59a4ece68eb95c667bac556f256c530
Ali Akrour <ali.akrour@gmail.com>
2021-06-21 07:00:44 +0000

    switches pods comm CNI plugin from flannel to calico

--- terraform/flannel-cni-plugin.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "flannel-cni-plugin" {
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/flannel-cni-plugin.yml -e 'ansible_become=yes ansible_become_user=vagrant ansible_become_pass=vagrant'"
-  }
-
-  depends_on = [null_resource.kubectl-remote]
-}
-
-
