143748e00e71ef8c2fec3cddec8a0527d42ba8ed
Ali Akrour <ali.akrour@gmail.com>
2021-06-27 16:47:36 +0000

    fix terraform format

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -4,7 +4,7 @@ resource "aws_route53_record" "A-record" {
   type    = "A"
 
   alias {
-    name                   = aws_lb.alb.dns_name 
+    name                   = aws_lb.alb.dns_name
     zone_id                = aws_lb.alb.zone_id
     evaluate_target_health = false
   }
