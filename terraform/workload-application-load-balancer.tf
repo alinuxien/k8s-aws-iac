08f32fe7c6129fcaf6fe421870b71a01a26ef055
Ali Akrour <ali.akrour@gmail.com>
2021-06-29 12:19:52 +0000

    changes application load balancer config

--- terraform/workload-application-load-balancer.tf
+++ terraform/workload-application-load-balancer.tf
@@ -44,6 +44,25 @@ resource "aws_security_group_rule" "out_https" {
   security_group_id = aws_security_group.alb.id
 }
 
+resource "aws_security_group_rule" "in_workload" {
+  type              = "ingress"
+  from_port         = 30000
+  to_port           = 32767
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb.id
+}
+
+resource "aws_security_group_rule" "out_workload" {
+  type              = "egress"
+  from_port         = 30000
+  to_port           = 32767
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.0/16"]
+  security_group_id = aws_security_group.alb.id
+}
+
+
 resource "aws_lb" "alb" {
   name                       = "alb"
   internal                   = false
@@ -94,17 +113,33 @@ resource "aws_lb_listener" "alb-listener-secure-wl" {
   }
 }
 
+resource "aws_lb_target_group" "tg-wl" {
+  name     = "tg-wl"
+  port     = 30000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_target_group_attachment" "tg-wl1" {
+  target_group_arn = aws_lb_target_group.tg-wl.arn
+  target_id        = aws_instance.worker-0.id
+  port             = 30000
+}
+
+resource "aws_lb_target_group_attachment" "tg-wl2" {
+  target_group_arn = aws_lb_target_group.tg-wl.arn
+  target_id        = aws_instance.worker-1.id
+  port             = 30000
+}
+
+
 resource "aws_lb_listener" "alb-listener-wl" {
   load_balancer_arn = aws_lb.alb.arn
-  port              = 80
+  port              = 30000
   protocol          = "HTTP"
   default_action {
-    type = "redirect"
-    redirect {
-      protocol    = "HTTPS"
-      port        = 443
-      status_code = "HTTP_302"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg-wl.arn
   }
 }
 
