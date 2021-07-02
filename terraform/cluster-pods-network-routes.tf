1d7e376a7c1dbc70fa835fe54741e2a1513d5489
Ali Akrour <ali.akrour@gmail.com>
2021-07-02 18:25:41 +0000

    cleaning resources & var names; improving comments; renaming files

--- terraform/cluster-pods-network-routes.tf
+++ terraform/cluster-pods-network-routes.tf
@@ -15,13 +15,13 @@ module "subnet_addrs" {
 }
 
 resource "aws_route" "worker-0-pods-route-to-other-node-pods" {
-  route_table_id         = aws_route_table.private-b.id
+  route_table_id         = aws_route_table.private-b-route-to-internet.id
   destination_cidr_block = module.subnet_addrs.networks[0].cidr_block
   instance_id            = aws_instance.worker-0.id
 }
 
 resource "aws_route" "worker-1-pods-route-to-other-node-pods" {
-  route_table_id         = aws_route_table.private-a.id
+  route_table_id         = aws_route_table.private-a-route-to-internet.id
   destination_cidr_block = module.subnet_addrs.networks[1].cidr_block
   instance_id            = aws_instance.worker-1.id
 }
