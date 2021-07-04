8f047497235593e5542b33361ca1a560e50a327e
Ali Akrour <ali.akrour@gmail.com>
2021-07-04 13:27:21 +0000

    adds alb and route53 resources files to git repo

---
+++ terraform/cluster-app-dns-registration.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_record" "app-dns-record" {
+  zone_id = data.aws_route53_zone.app-domain-primary-zone.zone_id
+  name    = var.app-domain
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.alb.dns_name
+    zone_id                = aws_lb.alb.zone_id
+    evaluate_target_health = false
+  }
+}
